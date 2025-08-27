5b1532bf1bcf7138934c0841f72e7b4693c4fac8
Y4h14 <yahyasadig2013@gmail.com>
2025-08-27 12:00:43 +0300

    Adds databse query tests

--- hono-auth/src/db/queries.test.ts
+++ hono-auth/src/db/queries.test.ts
@@ -1,15 +1,52 @@
-import { describe, expect, it} from 'bun:test'
+import { afterEach, beforeEach, describe, expect, it} from 'bun:test'
 import { insertUser } from './queries'
-import { dbConn } from './db'
+import { creatTestDb } from '../test/test.db'
+import { Database } from 'bun:sqlite'
+
+let db: Database;
+
+beforeEach(()=>{
+    db = creatTestDb();
+})
+afterEach(()=>{
+    db.close;
+})
 
 describe('insertUser', ()=>{
     it('Should insert a user into the database', async ()=>{
-        const db = dbConn()
         const email = 'testmail@yahoo.com';
         const password = 'supersecret22'        
         const userId = await insertUser(db, email, password);
         console.log(userId);
         
         expect(userId).toBeDefined();
+    })
+
+    it('should throw an error if an email already exitst', async ()=>{
+        const email = 'testmail@yahoo.com';
+        const password = 'somepass';
+    
+        await insertUser(db,email, password ) // inserting the email first time
+        try {
+            // trying to reinsert the same email
+            await insertUser(db,email, password )
+        } catch (error) {
+            expect(error).toBeInstanceOf(Error)
+            // @ts-ignore
+            expect(error.message).toMatch(/UNIQUE constraint failed/)
+        }
+    })
+
+    it('Should throw an error if password is empty', async() => {
+        const email = "test@yahoo.com"
+        const password = ''
+        try {
+        await insertUser(db, email, password)
+        }
+        catch (error) {
+            expect(error).toBeInstanceOf(Error);
+            // @ts-ignore
+            expect(error.message).toMatch(/password must not be empty/);
+        }
     })
 })
